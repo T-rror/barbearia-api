ccd0b413293d7af56a4af3062c08cc9a56b0eff8
t-rror <devmateusfernandes@gmail.com>
2025-07-06 23:18:10 -0300

    feat: estrutura inicial com NestJS, Prisma, PostgreSQL e módulo de autenticação

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,68 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { signinDTO, signUpDTO } from './dtos/auth';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(data: signUpDTO) {
+    const userAlreadyExists = await this.prismaService.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new UnauthorizedException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+  }
+
+  async signin(data: signinDTO) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const passwordMatch = await bcrypt.compare(data.password, user.password);
+
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accessToken = await this.jwtService.signAsync({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+
+    return {
+      accessToken, 
+    };
+  }
+}
