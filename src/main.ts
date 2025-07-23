fc91b65ed5aba72ce9478f55207c43adece0c47c
t-rror <devmateusfernandes@gmail.com>
2025-07-22 21:50:06 -0300

    feat(main.ts): cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); 
   app.enableCors({
-    origin: 'https://barbearia-dkgvtxoqk-mateus-projects-4fb9ed17.vercel.app', // ou '*' se quiser liberar geral (não recomendado para produção)
+    origin: 'https://barbearia-app-tau.vercel.app', // ou '*' se quiser liberar geral (não recomendado para produção)
     credentials: true, // se você estiver usando cookies/autenticação
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen('https://barbearia-app-tau.vercel.app');
 }
 bootstrap();
