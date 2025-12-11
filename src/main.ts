c0b7167e725d31fe86256c997ea79f460d1d775c
t-rror <devmateusfernandes@gmail.com>
2025-12-11 20:38:14 -0300

    (feat): cors new v

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://barbearia-hsax3kv0g-mateus-projects-4fb9ed17.vercel.app'
+      'https://barbearia-hsax3kv0g-mateus-projects-4fb9ed17.vercel.app',
+      'https://barbearia-deeahhyh5-mateus-projects-4fb9ed17.vercel.app'
       
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
