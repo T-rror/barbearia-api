798e05686892cb0e20adcc5b7bbcfe00cbb16889
t-rror <devmateusfernandes@gmail.com>
2025-12-21 18:32:51 -0300

    (cors|| )

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { AppointmentModule } from './appointment/appointment.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [AuthModule, AppointmentModule, PrismaModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
