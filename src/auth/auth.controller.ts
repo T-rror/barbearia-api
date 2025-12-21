7b7a8aa70d0d2508200715a70138afbc43b680df
t-rror <devmateusfernandes@gmail.com>
2025-12-21 18:38:22 -0300

    (cors||| )

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,15 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { signinDTO, signUpDTO } from './dtos/auth';
 import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
+
 import e from 'express';
 
 import { Public } from 'src/common/decorators/public.decorator';
 
-
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  
-  
+
   @Public()
   @Post('signup')
   async signup(@Body() body: signUpDTO) {
@@ -24,15 +22,9 @@ export class AuthController {
     return this.authService.signin(body);
   }
 
-  @UseGuards(AuthGuard) // aqui usamos o guard que verifica o token
-  @Get('me')
-  async me(@Req() req) {
-    return req.user;
-  } 
-   
   @Public()
-   @Post('check-email')
-   async checkemail(@Body('email')email: string) {
+  @Post('check-email')
+  async checkemail(@Body('email') email: string) {
     return this.authService.checkEmail(email);
-} 
+  }
 }
